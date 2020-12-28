251a2b7a74cb468b064db06b8ea7e98103283058
Anastasia-Krutitskaya <krut.a.b@yandex.ru>
2020-12-28 19:54:45 +0300

    changed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,64 +1,38 @@
+"use strict"
 function getResult(a,b,c){
-    "use strict"
-    let D;
-    let x = new Array();
-    D = Math.pow(b,2) - 4*a*c;
-    if ( D > 0 ) {
-        let x1,
-            x2;    
-        x1 = (-b + Math.pow(D,0.5))/(2*a);
-        x2 = (-b - Math.pow(D,0.5))/(2*a);
-        x.push(x1);
-        x.push(x2);   
+    let D = Math.pow(b,2) - 4*a*c;
+    let x = [];
+    if ( D > 0 ) {    
+        x.push((-b + Math.sqrt(D))/(2*a));
+        x.push((-b - Math.pow(D,0.5))/(2*a));   
     } else if ( D == 0 ) {
-        let x1;
-        x1 = (-b/(2*a));
-        x.push(x1);
-    } else {
-        x = [];
-    } 
+        x.push(-b/(2*a));
+    }    
     return x;
 
 }
 
 function getAverageMark(marks){
-    "use strict"
      if (marks.length == 0) {
         return 0;
      } else if (marks.length > 5) {
          console.log("Количество оценок больше 5");
-         marks.splice(5,marks.length-5);
-         countSum(marks.length);
-         
-     } else if (marks.length <= 5) {
-        countSum(marks.length);   
-     }
+         marks.splice(5);    
+     }   
+    
+    let sum = 0;
+    for ( let i = 0; i < marks.length; i ++) {
+        sum += marks[i];
+        }
 
-    function countSum(l) {
-        let result = 0;
-        for ( let i = 0; i < l; i ++) {
-            result = result + marks[i];
-            }
-        return result;
-    }
-
-    let sum = countSum(marks.length);
-    let averageMark = sum / marks.length;
-
-    return averageMark;
+    return sum / marks.length;
 
 }
 
 function askDrink(name,dateOfBirthday){
-    let userName = name;
-    let yearOfBirth = dateOfBirthday.getFullYear();
-    let today = new Date();
-    let thisYear = today.getFullYear();
-    let age = thisYear - yearOfBirth;
-    if (age >= 18) {
-        result = "Не желаете ли олд-фэшн, " + userName + "?";
-    } else if (age < 18) {
-        result = "Сожалею, " + userName + ", но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!"
+    if ( new Date().getFullYear() - dateOfBirthday.getFullYear() >= 18 ) {
+       return "Не желаете ли олд-фэшн, " + name + "?";
+    } else {
+       return "Сожалею, " + name + ", но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!"
     }
-    return result;
 }
