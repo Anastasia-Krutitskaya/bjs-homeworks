ffc6dda7345ef3403c705c29ceb021bc342d5bc1
Anastasia Krutitskaya <akrutitskaya@bk.ru>
2021-01-12 16:15:31 +0300

    first commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,43 @@
-//String.prototype.isPalindrome - для задачи №1
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function() {
+   let strLen = this.length;
+   let str = this.toUpperCase();
+   for (let i = 0; i < strLen; i++) {
+     if (str[i]=== str[strLen - 1 - i]) {
+       return true;
+     } else {
+         return false;
+     }
+   }
 }
 
+
+
+function getAverageMark(marks) {
+  if (marks.length == 0) {
+          return 0;
+       }
+    let sum = 0;
+    for ( let i = 0; i < marks.length; i ++) {
+        sum += marks[i];
+        }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
+}
+
+
+
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = Date.now();
+  let birthdayDate = Date.parse(birthday);
+  let diff = now - birthdayDate;
+  let age = diff/31557600000;
+  if (age > 18) {
+    return true
+  } else {
+    return false
+  }
+
 }
